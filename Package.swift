46c712925999c268e5a17933bb7cdfcc700e8e98
Mohamed Ghebaji <mohamed.ghebaji@radiofrance.com>
2022-11-08 09:50:19 +0100

    fix spm

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,18 +16,15 @@ let package = Package(
             targets: ["RxReachability"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift", .upToNextMajor(from: "6.0.0")),
-        .package(
-            name: "Reachability",
-            url: "https://github.com/ashleymills/Reachability.swift",
-            .upToNextMajor(from: "5.1.0")
-        )
+        .package(url: "https://github.com/ReactiveX/RxSwift", .upToNextMajor(from: "6.5.0")),
+        .package(url: "https://github.com/ashleymills/Reachability.swift", .upToNextMajor(from: "5.1.0")),
     ],
     targets: [
         .target(
             name: "RxReachability",
             dependencies: [
-                "RxSwift", "Reachability",
+                "RxSwift",
+                .product(name: "Reachability", package: "Reachability.swift"),
                 .product(name: "RxCocoa", package: "RxSwift")
         ]),
 
